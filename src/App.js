661d95a476df2e5e5417531b8c7883a246a4fcaa
YiTing07 <hevecandy@gmail.com>
2025-07-21 21:28:04 +0800

    chore: cleanup creat-app default files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Home />
       </header>
     </div>
   );
